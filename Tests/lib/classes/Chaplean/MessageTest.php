b3a488e1f9d8cad643eb147ba9d8a09cc237ad0c
Matthias Devlamynck <matthias@chaplean.com>
2016-06-16 11:39:35 +0200

    Add tests

--- Tests/lib/classes/Chaplean/MessageTest.php
+++ Tests/lib/classes/Chaplean/MessageTest.php
@@ -43,7 +43,7 @@ class MessageTest extends LogicalTest
         $chapleaConfig = $this->getContainer()->getParameter('chaplean_mailer');
         $message = new Message($chapleaConfig);
         $message->setSubject('My subject');
-        
+
         static::assertEquals('[TEST]My subject', $message->getSubject());
     }
 
@@ -112,7 +112,7 @@ class MessageTest extends LogicalTest
 
         $this->assertInternalType('float', $message->getTime());
     }
-    
+
     /**
      * @return void
      */
@@ -126,7 +126,7 @@ class MessageTest extends LogicalTest
             'other_to_transform@example.com',
             'named_to_transform@example.com' => 'named address',
         );
-        
+
         static::assertEquals(
             array(
                 'already_transformed@yopmail.com' => null,
@@ -135,6 +135,129 @@ class MessageTest extends LogicalTest
                 'named_to_transform_example_com@yopmail.com' => 'named address',
             ),
             $message->transformMail($addresses)
+        );
+    }
+
+    /**
+     * @return void
+     */
+    public function testSetToHandlesArray()
+    {
+        $chapleaConfig = $this->getContainer()->getParameter('chaplean_mailer');
+        $message = new Message($chapleaConfig);
+
+        $message->setTo(array('address@example.com'));
+        static::assertEquals(array('address_example_com@yopmail.com' => 'address@example.com'), $message->getTo());
+    }
+
+    /**
+     * @return void
+     */
+    public function testSetToHandlesSingleAddress()
+    {
+        $chapleaConfig = $this->getContainer()->getParameter('chaplean_mailer');
+        $message = new Message($chapleaConfig);
+
+        $message->setTo('address@example.com');
+        static::assertEquals(array('address_example_com@yopmail.com' => null), $message->getTo());
+    }
+
+    /**
+     * @return void
+     */
+    public function testSetToHandlesSingleAddressWithName()
+    {
+        $chapleaConfig = $this->getContainer()->getParameter('chaplean_mailer');
+        $message = new Message($chapleaConfig);
+
+        $message->setTo('address@example.com', 'my test address');
+        static::assertEquals(array('address_example_com@yopmail.com' => 'my test address'), $message->getTo());
+    }
+
+    /**
+     * @return void
+     */
+    public function testSetCcHandlesArray()
+    {
+        $chapleaConfig = $this->getContainer()->getParameter('chaplean_mailer');
+        $message = new Message($chapleaConfig);
+
+        $message->setCc(array('address@example.com'));
+        static::assertEquals(array('address_example_com@yopmail.com' => 'address@example.com'), $message->getCc());
+    }
+
+    /**
+     * @return void
+     */
+    public function testSetCcHandlesSingleAddress()
+    {
+        $chapleaConfig = $this->getContainer()->getParameter('chaplean_mailer');
+        $message = new Message($chapleaConfig);
+
+        $message->setCc('address@example.com');
+        static::assertEquals(array('address_example_com@yopmail.com' => null), $message->getCc());
+    }
+
+    /**
+     * @return void
+     */
+    public function testSetCcHandlesSingleAddressWithName()
+    {
+        $chapleaConfig = $this->getContainer()->getParameter('chaplean_mailer');
+        $message = new Message($chapleaConfig);
+
+        $message->setCc('address@example.com', 'my test address');
+        static::assertEquals(array('address_example_com@yopmail.com' => 'my test address'), $message->getCc());
+    }
+
+    /**
+     * @return void
+     */
+    public function testSetBccHandlesArray()
+    {
+        $chapleaConfig = $this->getContainer()->getParameter('chaplean_mailer');
+        $message = new Message($chapleaConfig);
+
+        $message->setBcc(array('address@example.com'));
+        static::assertEquals(
+            array(
+                'address_example_com@yopmail.com' => 'address@example.com',
+                'staff_chaplean_com@yopmail.com' => 'staff@chaplean.com'
+            ), $message->getBcc()
+        );
+    }
+
+    /**
+     * @return void
+     */
+    public function testSetBccHandlesSingleAddress()
+    {
+        $chapleaConfig = $this->getContainer()->getParameter('chaplean_mailer');
+        $message = new Message($chapleaConfig);
+
+        $message->setBcc('address@example.com');
+        static::assertEquals(
+            array(
+                'address_example_com@yopmail.com' => null,
+                'staff_chaplean_com@yopmail.com' => 'staff@chaplean.com'
+            ), $message->getBcc()
+        );
+    }
+
+    /**
+     * @return void
+     */
+    public function testSetBccHandlesSingleAddressWithName()
+    {
+        $chapleaConfig = $this->getContainer()->getParameter('chaplean_mailer');
+        $message = new Message($chapleaConfig);
+
+        $message->setBcc('address@example.com', 'my test address');
+        static::assertEquals(
+            array(
+                'address_example_com@yopmail.com' => 'my test address',
+                'staff_chaplean_com@yopmail.com' => 'staff@chaplean.com'
+            ), $message->getBcc()
         );
     }
 }
